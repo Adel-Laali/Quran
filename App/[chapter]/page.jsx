b1e32596be1d2f2743667d8d1c51559ca309c124
nefcanto <sn.1361@gmail.com>
2023-02-03 07:33:26 +0330

    committed by script

--- App/[chapter]/page.jsx
+++ App/[chapter]/page.jsx
@@ -2,47 +2,49 @@ import { useRouter } from 'next/router'
 
 const ChapterAndVerse = ({ chapter, errorCode }) => {
 
-    const router = useRouter()
+    return <h1>hi</h1>
 
-    const goToVerse = (verse) => {
-        router.push(`/${chapter.chapter_number}/${verse}`);
-    }
+    // const router = useRouter()
 
-    if (errorCode) {
-        return <h1>Error - {errorCode}</h1>
-    }
+    // const goToVerse = (verse) => {
+    //     router.push(`/${chapter.chapter_number}/${verse}`);
+    // }
 
-    return <>
-        <h1 className="flex py-4 text-5xl font-bold justify-center">{chapter.name_simple} <span className="text-sm ml-2">({chapter.chapter_number})</span></h1>
-        <h3 className="flex py-2 text-3xl font-medium justify-center">{chapter.name_arabic}</h3>
-        <ul id='verses' className="flex flex-wrap mx-12 mb-4 justify-center">
-            {
-                Array(chapter.verses_count).fill().map(
-                    (x, i) => <li key={i} onClick={() => goToVerse(i + 1)} className="p-4 m-2 bg-green-200 rounded hover:bg-green-900 hover:text-white transition-all duration-200 cursor-pointer w-16 flex items-center justify-center">
-                        {i + 1}
-                    </li>)
-            }
-        </ul>
-    </>
+    // if (errorCode) {
+    //     return <h1>Error - {errorCode}</h1>
+    // }
+
+    // return <>
+    //     <h1 className="flex py-4 text-5xl font-bold justify-center">{chapter.name_simple} <span className="text-sm ml-2">({chapter.chapter_number})</span></h1>
+    //     <h3 className="flex py-2 text-3xl font-medium justify-center">{chapter.name_arabic}</h3>
+    //     <ul id='verses' className="flex flex-wrap mx-12 mb-4 justify-center">
+    //         {
+    //             Array(chapter.verses_count).fill().map(
+    //                 (x, i) => <li key={i} onClick={() => goToVerse(i + 1)} className="p-4 m-2 bg-green-200 rounded hover:bg-green-900 hover:text-white transition-all duration-200 cursor-pointer w-16 flex items-center justify-center">
+    //                     {i + 1}
+    //                 </li>)
+    //         }
+    //     </ul>
+    // </>
 }
 
 const notFound = { props: { errorCode: 404 } };
 
-export async function getServerSideProps({ params, res }) {
-    const chapterNumber = params.chapter;
-    if (isNaN(chapterNumber * 1)) {
-        res.statusCode = 404;
-        return notFound;
-    }
-    if (chapterNumber * 1 > 114 || chapterNumber * 1 < 1) {
-        res.statusCode = 404;
-        return notFound;
-    }
+// export async function getServerSideProps({ params, res }) {
+//     const chapterNumber = params.chapter;
+//     if (isNaN(chapterNumber * 1)) {
+//         res.statusCode = 404;
+//         return notFound;
+//     }
+//     if (chapterNumber * 1 > 114 || chapterNumber * 1 < 1) {
+//         res.statusCode = 404;
+//         return notFound;
+//     }
 
-    const response = await fetch('https://api.quran.com/api/v3/chapters')
-    const chapters = await response.json()
-    const chapter = chapters.chapters.filter(i => i.chapter_number == chapterNumber)[0];
-    return { props: { chapter } };
-}
+//     const response = await fetch('https://api.quran.com/api/v3/chapters')
+//     const chapters = await response.json()
+//     const chapter = chapters.chapters.filter(i => i.chapter_number == chapterNumber)[0];
+//     return { props: { chapter } };
+// }
 
 export default ChapterAndVerse;
--- App/random/page.jsx
+++ App/random/page.jsx
@@ -1,34 +1,35 @@
 import { useEffect, useRef } from 'react'
-import quran from "../Contents/quran"
+import quran from "../../Contents/quran"
 
 const Random = ({ chapter, verse }) => {
+    return <h1>hi</h1>
 
-    const link = useRef()
+    // const link = useRef()
 
-    const reload = (e) => {
-        link.current.click()
-        setTimeout(() => {
-            window.location.reload()
-        }, 1000)
-        e.stopPropagation()
-        e.preventDefault()
-    }
+    // const reload = (e) => {
+    //     link.current.click()
+    //     setTimeout(() => {
+    //         window.location.reload()
+    //     }, 1000)
+    //     e.stopPropagation()
+    //     e.preventDefault()
+    // }
 
-    return <div onClick={(e) => reload()} className="flex w-screen h-screen items-center justify-center">
-        <a ref={link} className="text-5xl" target='_blank' href={`/${chapter}/${verse}`}>{chapter}:{verse}</a>
-    </div>
+    // return <div onClick={(e) => reload()} className="flex w-screen h-screen items-center justify-center">
+    //     <a ref={link} className="text-5xl" target='_blank' href={`/${chapter}/${verse}`}>{chapter}:{verse}</a>
+    // </div>
 }
 
 export default Random
 
-export async function getServerSideProps({ params, res }) {
-    const randomChapter = Math.ceil(Math.random() * 114)
-    const verseCount = quran.filter(i => i.chapter * 1 === randomChapter)[0].verseCount
-    const randomVerse = Math.ceil(Math.random() * verseCount)
-    return {
-        props: {
-            chapter: randomChapter,
-            verse: randomVerse
-        }
-    }
-}
+// export async function getServerSideProps({ params, res }) {
+//     const randomChapter = Math.ceil(Math.random() * 114)
+//     const verseCount = quran.filter(i => i.chapter * 1 === randomChapter)[0].verseCount
+//     const randomVerse = Math.ceil(Math.random() * verseCount)
+//     return {
+//         props: {
+//             chapter: randomChapter,
+//             verse: randomVerse
+//         }
+//     }
+// }
